4c28ec15d021ee468bdbf7deef9f915641300db1
Juliana Ransani <juliana.ransani.2@gmail.com>
2024-10-30 21:00:16 -0300

    Criando classe Tarefa e Usuário.

--- src/main/java/com/ransani/gerenciamentotarefas/Entidade/Tarefa.java
+++ src/main/java/com/ransani/gerenciamentotarefas/Entidade/Tarefa.java
@@ -1,6 +1,27 @@
 package com.ransani.gerenciamentotarefas.Entidade;
 
+
+import java.util.Date;
+
 public class Tarefa {
+
+    private long id;
+    private String titulo;
+    private String descricao;
+    private Date praso;
+    private Date cadastro;
+    private Status status;
+    private Usuario responsavel;
+
+    public enum Status{
+        PENDENTE,
+        EM_ANDAMENTO,
+        ATRASADA,
+        CONCLUIDA
+    }
+
+
+
 
 
 }
---
+++ src/main/java/com/ransani/gerenciamentotarefas/Entidade/Usuario.java
@@ -0,0 +1,8 @@
+package com.ransani.gerenciamentotarefas.Entidade;
+
+public class Usuario {
+
+    private long id;
+    private String nome;
+    private String email;
+}
