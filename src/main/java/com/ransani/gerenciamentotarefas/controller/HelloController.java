9f013078f440ef8f7a46eacd3da49615d070b8da
Juliana Ransani <juliana.ransani.2@gmail.com>
2024-11-10 23:15:19 -0300

    criando novo menu

--- src/main/java/com/ransani/gerenciamentotarefas/controller/HelloController.java
+++ src/main/java/com/ransani/gerenciamentotarefas/controller/HelloController.java
@@ -1,14 +1,48 @@
 package com.ransani.gerenciamentotarefas.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button startButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void onMenuClick() {
+        try {
+            // Carrega o arquivo FXML do menu
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/ransani/gerenciamentotarefas/menu.fxml"));
+            Parent root = fxmlLoader.load();
+
+            // Obtém a janela atual e substitui a cena
+            Stage stage = (Stage) startButton.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            centerStage(stage);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void centerStage(Stage stage) {
+        double screenWidth = Screen.getPrimary().getBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getBounds().getHeight();
+
+        double windowWidth = stage.getWidth();
+        double windowHeight = stage.getHeight();
+
+        double x = (screenWidth - windowWidth) / 2;
+        double y = (screenHeight - windowHeight) / 2;
+
+        stage.setX(x);
+        stage.setY(y);
     }
 }
---
+++ src/main/java/com/ransani/gerenciamentotarefas/controller/MenuController.java
@@ -0,0 +1,15 @@
+package com.ransani.gerenciamentotarefas.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MenuController {
+
+	@FXML
+    public void onUsuarioClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void onTarefaClick(ActionEvent actionEvent) {
+    }
+}
