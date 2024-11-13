11dfe5d60668e432aa7f4ec0549367e99e217b73
Juliana Ransani <juliana.ransani.2@gmail.com>
2024-11-13 16:00:41 -0300

    Menu de tarefas

--- src/main/java/com/ransani/gerenciamentotarefas/controller/MenuController.java
+++ src/main/java/com/ransani/gerenciamentotarefas/controller/MenuController.java
@@ -2,14 +2,53 @@ package com.ransani.gerenciamentotarefas.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MenuController {
+    @FXML
+    private Button usuariotButton;
 
 	@FXML
     public void onUsuarioClick(ActionEvent actionEvent) {
+
     }
 
     @FXML
     public void onTarefaClick(ActionEvent actionEvent) {
+        try {
+            // Carrega o arquivo FXML do menu
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/ransani/gerenciamentotarefas/menu-tarefas.fxml"));
+            Parent root = fxmlLoader.load();
+
+            // Obtém a janela atual e substitui a cena
+            Stage stage = (Stage) usuariotButton.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            centerStage(stage);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void centerStage(Stage stage) {
+        double screenWidth = Screen.getPrimary().getBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getBounds().getHeight();
+
+        double windowWidth = stage.getWidth();
+        double windowHeight = stage.getHeight();
+
+        double x = (screenWidth - windowWidth) / 2;
+        double y = (screenHeight - windowHeight) / 2;
+
+        stage.setX(x);
+        stage.setY(y);
     }
 }
---
+++ src/main/java/com/ransani/gerenciamentotarefas/controller/MenuTarefas.java
@@ -0,0 +1,22 @@
+package com.ransani.gerenciamentotarefas.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MenuTarefas {
+
+    @FXML
+    public void onCadastroTClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void onBuscarTClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void onStatusTClick(ActionEvent actionEvent) {
+    }
+    @FXML
+    public void onDeletarTClick(ActionEvent actionEvent) {
+    }
+    
+
+}
